95607a963b383478fdce4e589033e2d06feda5b1
Johuotar <jmh@dnainternet.net>
2021-12-16 16:00:35 +0200

    improved carousel, exit btn placeholder, background

--- Scenes/Credits.gd
+++ Scenes/Credits.gd
@@ -1,8 +1,9 @@
 extends Control
 
 export (NodePath) onready var carousel = get_node(carousel) as Control
-export (NodePath) onready var cardTimer = get_node(cardTimer) as Timer
 var cards = []
+var active_card
+var card_index = 0
 export (Curve) var cardSpeedCurve
 var moveAmount
 var viewportX = OS.window_size.x
@@ -10,12 +11,24 @@ var viewportX = OS.window_size.x
 func _ready():
 	for card in carousel.get_children():
 		cards.append(card)
+	active_card = cards[card_index]
 
 func _process(delta):
-	moveAmount = cardSpeedCurve.interpolate(cardTimer.time_left/10)
-	for card in cards:
-		if card.rect_position.x > viewportX + 1:
-			card.rect_position.x = card.startXPos # TODO: FIX: cards dont stay in sync and will begin overlapping
-		else:
-			card.rect_position.x += moveAmount
+		moveAmount = abs(viewportX/2 - active_card.rect_position.x - active_card.rect_size.x*active_card.rect_scale.x/2)
+		moveAmount = moveAmount / 20
+		if moveAmount < 5:
+			moveAmount = 0.5
+		if active_card.rect_position.x > viewportX + 1:
+			active_card.rect_position.x = active_card.startXPos
+			 # TODO: FIX: cards dont stay in sync and will begin overlapping
+			if !card_index + 1 == cards.size():
+				card_index += 1
+			else:
+				card_index = 0
+			active_card = cards[card_index]
+		active_card.rect_position.x += moveAmount
 		
+
+
+func _on_ExitBtn_pressed():#TODO Exit credits into main menu or straight into gameplay
+	print("Exiting credits not implemented. Enjoy watching them!")
